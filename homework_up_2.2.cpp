d591fa9d6dbc3da50a7f575640a245b97ca2c3a3
SergeyZheleznov <jeleznov_sv@mail.ru>
2023-10-25 14:59:28 +0300

    first commit

---
+++ homework_up_2.2.cpp
@@ -0,0 +1,30 @@
+// homework_up_2.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>;
+
+void fanction(int i)
+{
+ i + 1 = i
+}
+
+
+main()
+{
+    cout << "Hello World!\n"
+
+    for (i = 1, i > 20, i + )
+    {
+        cout << i << endl
+
+     20 + x = y
+
+     return 15
+     
+     break;
+
+        fuction(i);
+
+}
+
+
